ea1c1e665a7b782c08a55d6cca80befffbe7214e
devin-lynch <devin.l.lynch@gmail.com>
2023-01-24 18:43:44 -0600

    Add search/filter functionality

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -1,9 +1,14 @@
 // import React from "react";
 
-const SearchBox = () => {
+const SearchBox = ({ searchChange }) => {
     return (
-        <div>
-            <input type="search" placeholder="search robots..." />
+        <div className="pa2">
+            <input 
+                className="pa3 ba b--green bg-lightest-blue"
+                type="search" 
+                placeholder="search robots..." 
+                onChange={searchChange}
+            />
         </div>
     )
 }
